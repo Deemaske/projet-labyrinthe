acbc26487ed9f85171cddee3fc345a3cb4e86946
Deemaské <yoel.richard@gmail.com>
2023-05-15 11:15:50 +0200

    ajout du timer

--- interface_graphique.py
+++ interface_graphique.py
@@ -1,6 +1,7 @@
 from creation_labyrinthe import labyrinthe,Tuile
 from PIL import Image
 import pygame
+import time
 
 def dessiner(tableau):
     """Permet de dessiner un labyrinthe à partir d'une liste de liste de tuiles
@@ -28,20 +29,27 @@ def dessiner(tableau):
                 image.putpixel((x*3,y*3+1),couleur)
     return image
 
-img = dessiner(labyrinthe(15))
+img = dessiner(labyrinthe(10))
 img = img.resize((600,600),Image.Resampling.BOX)
 img.save("bg.png", format="png")
 
 pygame.init()
 fond = pygame.image.load("bg.png")
-screen = pygame.display.set_mode(fond.get_size())
+texte = pygame.font.Font(None,60)
+debut = time.time()
+dimension = fond.get_size()[0], fond.get_size()[1] + texte.render("0", False, (0,0,0)).get_size()[1]
+screen = pygame.display.set_mode(dimension)
 fond = fond.convert()
 screen.blit(fond,(0,0))
 
 continuer = True
 mouvement = False
+gagner = False
 while continuer:
     pygame.display.flip()
+    screen.fill((0,0,0))
+    screen.blit(fond,(0,0))
+    screen.blit(texte.render(f"{int(time.time()-debut)//60 % 60:02}:{int(time.time()-debut)%60:02}", False, (255,255,255)), (240,fond.get_size()[1]))
     for event in pygame.event.get():
         if event.type == pygame.QUIT or event.type == 771:
             continuer = False
@@ -52,7 +60,17 @@ while continuer:
             mouvement = False
         elif fond.get_at(pygame.mouse.get_pos()) == (255,0,0,255):
             continuer = False
-            print("Vous avez gagné!")
+            gagner = True
+
+screen.fill((0,0,0))
+if gagner:
+    screen.blit(texte.render(f"Vous avez gagné en {int(time.time()-debut)//60 % 60:02}:{int(time.time()-debut)%60:02}",False,(255,255,255)),(50,300))
+
+continuer = True
+while continuer:
     pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or event.type == 771:
+            continuer = False
 
 pygame.quit()
